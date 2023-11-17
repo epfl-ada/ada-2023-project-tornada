f85287b14854e771ff621fbda2d9a0b06b6271f7
Kajotello <112871989+Kajotello@users.noreply.github.com>
2023-11-17 23:45:56 +0100

    Improvments in notebook

---
+++ helpers.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+def parse_reviews_to_pickle(path, output, is_first):
+
+    all_reviews = list()
+    review_dict = dict()
+
+    with open("data/RateBeer/reviews.txt") as infile:
+        for i, line in enumerate(infile):
+            if line == '\n':
+                if not is_first and i < 60_000_000:
+                    continue
+                all_reviews.append(review_dict)
+                review_dict = dict()
+                if is_first and i > 60_000_000:
+                    break
+            else:
+                line_splited = line.split(':')
+                feature = line_splited[0]
+                value = ''.join(line_splited[1:])
+                if feature != 'text' and feature != 'review':
+                    review_dict[feature] = value.strip()
+        if review_dict:
+            all_reviews.append(review_dict)
+
+    df = pd.DataFrame(all_reviews)
+    df = df.astype({
+        'beer_name': 'str', 
+        'beer_id': 'int',
+        'brewery_name':'str',
+        'brewery_id': 'int',
+        'style': 'str',
+        'abv': 'float',
+        'date': 'int',
+        'user_id': 'int',
+        'appearance': 'int',
+        'aroma': 'int',
+        'palate': 'int',
+        'taste': 'int',
+        'overall': 'int',
+        'rating': 'float',
+        })
+    df['date'] = pd.to_datetime(df['date'], unit='s')
+
+    df.to_pickle(f'{output}/reviews_{int(is_first)}')
+
+
+
+def parse_reviews(path, output):
+    parse_reviews_to_pickle(path, output, True)
+    parse_reviews_to_pickle(path, output, False)
+
+    reviews_df0 = pd.read_pickle(f'{output}/reviews_0')
+    reviews_df1 = pd.read_pickle(f'{output}/reviews_1')
+
+    return pd.concat([reviews_df0, reviews_df1])
+
