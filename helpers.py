b0024b88de409aff414e22fa27b79daeb40591f4
kajotello <kajotello@gmail.com>
2023-12-22 23:59:01 +0100

    final commit

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def parse_reviews_to_pickle(path, output, is_first):
 
@@ -72,22 +73,50 @@ def parse_reviews(path, output):
     return pd.concat([reviews_df0, reviews_df1])
 
 
+def get_beer_style_mapping():
+    return {
+    "Bocks": ["Bock", "Doppelbock", "Eisbock", "Maibock", "Weizenbock"],
+    "Brown Ales": ["Altbier", "American Brown Ale", "Belgian Dark Ale", "English Brown Ale", "English Dark Mild Ale"],
+    "Dark Ales": ["Dubbel", "Roggenbier", "Scottish Ale", "Winter Warmer"],
+    "Dark Lagers": ["American Amber / Red Lager", "Czech Amber Lager", "Czech Dark Lager", "European Dark Lager", "Märzen", "Munich Dunkel","Euro Dark Lager", "Rauchbier", "Schwarzbier", "Vienna Lager"],
+    "Hybrid Beers": ["Bière de Champagne / Bière Brut", "Braggot", "California Common / Steam Beer", "Cream Ale"],
+    "India Pale Ales": ["American IPA", "Belgian IPA", "Black IPA", "Brut IPA", "English IPA", "Imperial IPA", "Milkshake IPA", "New England IPA"],
+    "Pale Ales": ["American Amber / Red Ale", "American Blonde Ale", "American Pale Ale", "Belgian Blonde Ale", "Belgian Pale Ale", "Bière de Garde", "English Bitter", "English Pale Ale", "English Pale Mild Ale", "Extra Special / Strong Bitter (ESB)", "Grisette", "Irish Red Ale", "Kölsch", "Saison"],
+    "Pale Lagers": ["American Adjunct Lager","Dortmunder / Export Lager", "Euro Strong Lager", "American Pale Lager", "Czech Pilsener", "German Pilsener", "American Lager", "Bohemian / Czech Pilsner", "Czech Pale Lager", "European / Dortmunder Export Lager", "European Pale Lager","Euro Pale Lager", "European Strong Lager", "Festbier / Wiesnbier", "German Pilsner", "Helles", "Imperial Pilsner", "India Pale Lager (IPL)", "Kellerbier / Zwickelbier", "Light Lager", "Malt Liquor"],
+    "Porters": ["American Porter", "Baltic Porter", "English Porter", "Imperial Porter", "Robust Porter", "Smoked Porter"],
+    "Specialty Beers": ["Chile Beer","American Black Ale","Black & Tan", "Pumpkin Ale", "Herbed / Spiced Beer","Low Alcohol Beer", "Fruit / Vegetable Beer", "Fruit and Field Beer", "Gruit / Ancient Herbed Ale", "Happoshu", "Herb and Spice Beer", "Japanese Rice Lager", "Kvass", "Low-Alcohol Beer", "Pumpkin Beer", "Rye Beer", "Sahti", "Smoked Beer"],
+    "Stouts": ["American Imperial Stout","Milk / Sweet Stout","American Double / Imperial Stout", "American Stout", "English Stout", "Foreign / Export Stout", "Irish Dry Stout", "Oatmeal Stout", "Russian Imperial Stout", "Sweet / Milk Stout"],
+    "Strong Ales": ["American Barleywine","Belgian Strong Pale Ale","Belgian Strong Dark Ale", "American Strong Ale", "Belgian Dark Strong Ale", "Belgian Pale Strong Ale", "English Barleywine", "English Strong Ale", "Imperial Red Ale", "Old Ale", "Quadrupel (Quad)", "Scotch Ale / Wee Heavy", "Tripel", "Wheatwine"],
+    "Wheat Beers": ["American Dark Wheat Beer","American Pale Wheat Ale", "American Dark Wheat Ale", "Kristalweizen", "American Pale Wheat Beer", "Dunkelweizen", "Grodziskie", "Hefeweizen", "Kristallweizen", "Witbier"],
+    "Wild/Sour Beers": ["Berliner Weisse","Berliner Weissbier", "Brett Beer", "Faro", "Flanders Oud Bruin", "Flanders Red Ale", "Fruit Lambic", "Fruited Kettle Sour", "Gose", "Gueuze", "Lambic", "Wild Ale"]
+    }
 
-beer_style_mapping = {
-"Bocks": ["Bock", "Doppelbock", "Eisbock", "Maibock", "Weizenbock"],
-"Brown Ales": ["Altbier", "American Brown Ale", "Belgian Dark Ale", "English Brown Ale", "English Dark Mild Ale"],
-"Dark Ales": ["Dubbel", "Roggenbier", "Scottish Ale", "Winter Warmer"],
-"Dark Lagers": ["American Amber / Red Lager", "Czech Amber Lager", "Czech Dark Lager", "European Dark Lager", "Märzen", "Munich Dunkel","Euro Dark Lager", "Rauchbier", "Schwarzbier", "Vienna Lager"],
-"Hybrid Beers": ["Bière de Champagne / Bière Brut", "Braggot", "California Common / Steam Beer", "Cream Ale"],
-"India Pale Ales": ["American IPA", "Belgian IPA", "Black IPA", "Brut IPA", "English IPA", "Imperial IPA", "Milkshake IPA", "New England IPA"],
-"Pale Ales": ["American Amber / Red Ale", "American Blonde Ale", "American Pale Ale", "Belgian Blonde Ale", "Belgian Pale Ale", "Bière de Garde", "English Bitter", "English Pale Ale", "English Pale Mild Ale", "Extra Special / Strong Bitter (ESB)", "Grisette", "Irish Red Ale", "Kölsch", "Saison"],
-"Pale Lagers": ["American Adjunct Lager","Dortmunder / Export Lager", "Euro Strong Lager", "American Pale Lager", "Czech Pilsener", "German Pilsener", "American Lager", "Bohemian / Czech Pilsner", "Czech Pale Lager", "European / Dortmunder Export Lager", "European Pale Lager","Euro Pale Lager", "European Strong Lager", "Festbier / Wiesnbier", "German Pilsner", "Helles", "Imperial Pilsner", "India Pale Lager (IPL)", "Kellerbier / Zwickelbier", "Light Lager", "Malt Liquor"],
-"Porters": ["American Porter", "Baltic Porter", "English Porter", "Imperial Porter", "Robust Porter", "Smoked Porter"],
-"Specialty Beers": ["Chile Beer","American Black Ale","Black & Tan", "Pumpkin Ale", "Herbed / Spiced Beer","Low Alcohol Beer", "Fruit / Vegetable Beer", "Fruit and Field Beer", "Gruit / Ancient Herbed Ale", "Happoshu", "Herb and Spice Beer", "Japanese Rice Lager", "Kvass", "Low-Alcohol Beer", "Pumpkin Beer", "Rye Beer", "Sahti", "Smoked Beer"],
-"Stouts": ["American Imperial Stout","Milk / Sweet Stout","American Double / Imperial Stout", "American Stout", "English Stout", "Foreign / Export Stout", "Irish Dry Stout", "Oatmeal Stout", "Russian Imperial Stout", "Sweet / Milk Stout"],
-"Strong Ales": ["American Barleywine","Belgian Strong Pale Ale","Belgian Strong Dark Ale", "American Strong Ale", "Belgian Dark Strong Ale", "Belgian Pale Strong Ale", "English Barleywine", "English Strong Ale", "Imperial Red Ale", "Old Ale", "Quadrupel (Quad)", "Scotch Ale / Wee Heavy", "Tripel", "Wheatwine"],
-"Wheat Beers": ["American Dark Wheat Beer","American Pale Wheat Ale", "American Dark Wheat Ale", "Kristalweizen", "American Pale Wheat Beer", "Dunkelweizen", "Grodziskie", "Hefeweizen", "Kristallweizen", "Witbier"],
-"Wild/Sour Beers": ["Berliner Weisse","Berliner Weissbier", "Brett Beer", "Faro", "Flanders Oud Bruin", "Flanders Red Ale", "Fruit Lambic", "Fruited Kettle Sour", "Gose", "Gueuze", "Lambic", "Wild Ale"]
-}
 
-restructured_dict = {style: family for family, style in beer_style_mapping.items() for style in style} # Inversing keys and values 
+def map_style_to_family(style, restructured_dict):
+
+    '''
+    Checks if beer substyle string is conatined in the key of a dictionary mapping
+    and returns the family style value 
+
+    Parameters:
+    substyle (str): Beer style
+    restructured_dict (dict): Dictionary with key = style : value = family
+    
+    Returns:
+    family (str): Family of the beer style
+    
+
+    '''
+    
+    # Iterating through the keys
+     
+    for key, value in restructured_dict.items():
+        if key in style:
+            return value
+        
+        elif 'IPA' in style:
+
+            return 'India Pale Ales'
+        
+    
+    return np.NAN  # or any default value you prefer
